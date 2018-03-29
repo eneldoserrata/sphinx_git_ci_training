172d58590447ccaa5a23a31bcf06bc15f2823fcc
Eneldo Serrata <eneldoserrata@gmail.com>
2018-03-29 10:53:49 -0400

    ready for PR

---
+++ docs/build/epub/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0.1',
+    LANGUAGE: 'es',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.xhtml',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{"\u00edndic":0,busqued:0,modul:0,pagin:0},titles:["Welcome to sphinx_git_ci_training\u2019s documentation!"],titleterms:{and:0,documentation:0,indic:0,sphinx_git_ci_training:0,tabl:0,welcom:0}})
